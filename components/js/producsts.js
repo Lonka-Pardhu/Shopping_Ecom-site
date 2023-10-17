5a6c7bc39f4dc66c8e90ed76edf1da43f86a7ac7
Lonka-Pardhu <lonkapardhu99@gmail.com>
2023-10-17 18:41:38 +0200

    url assign removed

--- components/js/producsts.js
+++ components/js/producsts.js
@@ -71,42 +71,17 @@ function createProductCard(productData) {
 
     });
 }
-function isProductUrl() {
-    return window.location.href.includes('?product=');
-}
 
-function renderProductDetailsPage() {
-    document.querySelector('.productPage').style.display = 'block';
-    storedData = JSON.parse(window.localStorage.getItem('apiData'));
-    data = storedData;
+document.getElementById('feed').style.display = 'block';
+getProducts('https://nextjs-boilerplate-sgunique.vercel.app/api/products', createProductCard)
 
-    var productId = JSON.parse(window.localStorage.getItem('productId'))
-    loader.style.display = 'none'
-
-    const pageProductImage = document.getElementById('pageProductImage');
-    pageProductImage.src = data.result[productId].image;
-
-    const pageProductName = document.getElementById('pageProductName');
-    pageProductName.innerHTML = data.result[productId].name;
-
-    const pageProductDescription = document.getElementById('pageProductDescription');
-    pageProductDescription.innerHTML = data.result[productId].description;
-
-    const pageProductPrice = document.getElementById('pageProductPrice');
-    pageProductPrice.innerHTML = data.result[productId].price;
-
-    const pageProductRating = document.getElementById('pageProductRating');
-    pageProductRating.innerHTML = data.result[productId].rating;
-
-    const pageProductReviews = document.getElementById('pageProductReviews');
-    pageProductReviews.innerHTML = data.result[productId].reviews;
-
-}
-if (isProductUrl()) {
-    document.getElementById('feed').style.display = 'none';
-    renderProductDetailsPage();
-} else {
-    document.getElementById('feed').style.display = 'block';
-    getProducts('https://nextjs-boilerplate-sgunique.vercel.app/api/products', createProductCard)
+function addRouteToProduct() {
+    var element = document.querySelectorAll('.product');
+    element.forEach(ele => {
+        ele.addEventListener('click', () => {
+            window.localStorage.setItem('productId', ele.id)
+            renderProductDetailsPage();
+        })
+    });
 }
 
--- components/js/routing.js
+++ components/js/routing.js
@@ -1,18 +1,30 @@
-
-var route = '?product=';
-
-function addRouteToProduct() {
-    var element = document.querySelectorAll('.product');
-    element.forEach(ele => {
-        ele.addEventListener('click', () => {
-            let url = '';
-            let urlComponents = urlLink.split('?');
-            url = urlComponents[0] + route + ele.id;
-            window.localStorage.setItem('productId', ele.id)
-            window.location.assign(url)
-            productId = urlLink.slice(-1);
-            return productId;
-        })
-    });
-}
 addRouteToProduct()
+
+function renderProductDetailsPage() {
+    document.getElementById('feed').style.display = 'none';
+    document.querySelector('.productPage').style.display = 'block';
+    storedData = JSON.parse(window.localStorage.getItem('apiData'));
+    data = storedData;
+
+    var productId = JSON.parse(window.localStorage.getItem('productId'))
+    loader.style.display = 'none'
+
+    const pageProductImage = document.getElementById('pageProductImage');
+    pageProductImage.src = data.result[productId].image;
+
+    const pageProductName = document.getElementById('pageProductName');
+    pageProductName.innerHTML = data.result[productId].name;
+
+    const pageProductDescription = document.getElementById('pageProductDescription');
+    pageProductDescription.innerHTML = data.result[productId].description;
+
+    const pageProductPrice = document.getElementById('pageProductPrice');
+    pageProductPrice.innerHTML = data.result[productId].price;
+
+    const pageProductRating = document.getElementById('pageProductRating');
+    pageProductRating.innerHTML = data.result[productId].rating;
+
+    const pageProductReviews = document.getElementById('pageProductReviews');
+    pageProductReviews.innerHTML = data.result[productId].reviews;
+
+}
