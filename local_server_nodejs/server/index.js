c9eed1dbf6894764e0e079a8166c729637708478
Lonka-Pardhu <lonkapardhu99@gmail.com>
2023-11-03 16:59:40 +0100

    creating file to save user data for every user that creates account

--- local_server_nodejs/server/index.js
+++ local_server_nodejs/server/index.js
@@ -2,31 +2,42 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 const fs = require('fs');
-const cors = require('cors');
 
-app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/formData', function (req, res) {
-    const data = JSON.parse(JSON.stringify(req.body));
 
-    fs.readFile('userData.json', 'utf8', function (err, fileData) {
+    const data = req.body;
+    let stringData = JSON.stringify(data);
+    var fileName = `${data.first_name}.json`
 
+
+    fs.appendFile(fileName, stringData, function (err) {
         if (err) {
-            console.error('Error reading data file:', err);
+            console.log(err);
+        } else {
+            console.log('user data file created successfully..!')
         }
-        let userData = JSON.parse(fileData);
-
-        userData.push(data);
-
-        fs.writeFile('userData.json', JSON.stringify(userData), (err) => {
-            if (err) {
-                console.error(err);
-            }
-        })
     })
-    console.log('Received Form Data:', data);
-    console.log('user data/details successfully added to userData.json file')
+
+    // fs.readFile('userData.json', 'utf8', function (err, fileData) {
+
+    //     if (err) {
+    //         console.error('Error reading data file:', err);
+    //     }
+    //     let userData = JSON.parse(fileData);
+
+    //     userData.push(data);
+
+    //     fs.writeFile('userData.json', JSON.stringify(userData), (err) => {
+    //         if (err) {
+    //             console.error(err);
+    //         }
+    //     })
+    // })
+
+    // console.log('Received Form Data:', data);
 
     res.redirect('http://127.0.0.1:5500/components/products.html');
 });
