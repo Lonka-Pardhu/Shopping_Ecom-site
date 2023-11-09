71c9a8d2168c319b760940469b6cd5fc64dec3e7
Lonka-Pardhu <lonkapardhu99@gmail.com>
2023-11-09 18:37:25 +0100

    form separation/other changes

--- client/js/forms.js
+++ client/js/forms.js
@@ -1,32 +1,3 @@
-// var formSubmitted = false;
-// window.localStorage.setItem('formSubmitted', JSON.stringify(formSubmitted))
-
-
-// var formDisplay = document.querySelector('.form-display');
-// var submitForm = document.querySelector('.submit-button')
-// var isFormSubmitted = JSON.parse(window.localStorage.getItem('formSubmitted'));
-
-// function displayForm() {
-//     if (isFormSubmitted) {
-//
-//         formDisplay.style.display = 'none';
-//     } else {
-//         blockLayer.style.display = 'block';
-//         formDisplay.style.display = 'block';
-//     }
-// }
-// displayForm();
-
-// let closeForm = document.querySelector('.ask-me-later-btn')
-// closeForm.addEventListener('click', () => {
-//
-//     formDisplay.style.display = 'none';
-// });
-
-// submitForm.addEventListener('click', () => {
-//     window.localStorage.setItem('formSubmitted', 'true');
-// })
-
 const formEle = document.getElementById('account-form');
 
 formEle.addEventListener('submit', (e) => {
@@ -34,7 +5,6 @@ formEle.addEventListener('submit', (e) => {
 
     const formData = new FormData(formEle);
     const details = new URLSearchParams(formData);
-    // console.log(details);
 
     fetch('/register', {
         method: 'POST',
@@ -56,43 +26,6 @@ formEle.addEventListener('submit', (e) => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function validateForm(event) {
-
-//     event.preventDefault();
-//     var form = document.getElementById('account-form');
-//     let firstName = document.getElementById('first-name').value;
-//     let lastName = document.getElementById('last-name').value;
-//     const formData = new FormData(form);
-
-//     formData.append('first_name', firstName);
-//     formData.append('last_name', lastName)
-
-//     console.log('stopping form from submission')
-//     console.log(formData);
-// }
-// validateForm();
-
 // *? take control of form
 // *! prevent user form entering product page directly , check user is authenticate, or redirect to login page directly
 // ** use FormData object to append data to form data
@@ -100,7 +33,7 @@ formEle.addEventListener('submit', (e) => {
 // ** use cookie to check user login and lcear cookie after closing borwser
 // ** user check login through saving user data in cookie
 // ** initial login page
-//  TODO:  form 
+//  TODO:  form
 
 
 
@@ -120,19 +53,19 @@ formEle.addEventListener('submit', (e) => {
 
 
 
-var accountForm = document.querySelector('.register-form');
-var openLoginForm = document.querySelector('.login-span')
-var loginForm = document.querySelector('.login-form');
-var openCreateAccForm = document.querySelector('.register-span')
+// var accountForm = document.querySelector('.register-form');
+// var openLoginForm = document.querySelector('.login-span')
+// var loginForm = document.querySelector('.login-form');
+// var openCreateAccForm = document.querySelector('.register-span')
 
-var popup = document.getElementById('response');
+// var popup = document.getElementById('response');
 
-openLoginForm.addEventListener('click', () => {
-    loginForm.style.display = 'block';
-    accountForm.style.display = 'none';
-})
+// openLoginForm.addEventListener('click', () => {
+//     loginForm.style.display = 'block';
+//     accountForm.style.display = 'none';
+// })
 
-openCreateAccForm.addEventListener('click', () => {
-    accountForm.style.display = 'block';
-    loginForm.style.display = 'none';
-})
+// openCreateAccForm.addEventListener('click', () => {
+//     accountForm.style.display = 'block';
+//     loginForm.style.display = 'none';
+// })
