bbcd93936d60e135c62bd2501f55d35db35a88aa
Lonka-Pardhu <lonkapardhu99@gmail.com>
2023-11-22 12:54:28 +0100

    email err >> realtime

--- client/js/registerForm.js
+++ client/js/registerForm.js
@@ -1,22 +1,27 @@
 let openLoginFormOne = document.querySelector('.login-span');
 let openLoginFormTwo = document.querySelector('.login-page-button')
-let closeResponseMessage = document.querySelector('.close-response-message')
+let closeResponseMessage = document.querySelector('.close-response-message');
 let timerSpanOne = document.querySelector('.counter-display-one');
 const registerForm = document.getElementById('account-form');
 const passwordValidationRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 const emailValidationRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})$/;
 let emailInput = document.querySelector('.email-input');
-let emailValidationErr = document.querySelector('.email-validation-err')
+let emailValidationErr = document.querySelector('.email-validation-err');
 
 let timeout;
 emailInput.addEventListener('input', () => {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
         var enteredEmail = emailInput.value.trim();
-        if (emailValidationRegex.test(enteredEmail) || emailInput.value === '') {
+        if (emailValidationRegex.test(enteredEmail)) {
             emailValidationErr.style.display = 'none';
+            console.log('running')
+        } else if (emailInput.value === '') {
+            emailValidationErr.style.display = 'block';
+            emailValidationErr.innerHTML = 'This field is required.';
         } else {
             emailValidationErr.style.display = 'block';
+            emailValidationErr.innerHTML = 'Please enter a valid email address (Ex: user@example.com)';
         }
     }, 1000);
 })
@@ -35,17 +40,17 @@ registerForm.addEventListener('submit', (e) => {
     if (userFirstName === '') {
         window.alert('Please enter First Name');
     } else if (userLastName === '') {
-        window.alert('Please enter your Last Name')
+        window.alert('Please enter your Last Name');
     } /*else if (userEmail === '') {
         window.alert('Please enter your Email.')
     } */else if (userDob === '') {
         window.alert('Please enter your Date of birth.')
     } else if (!document.querySelector('input[name="gender"]:checked')) {
-        window.alert('Please select your Gender.')
+        window.alert('Please select your Gender.');
     } else if (userPassword === '') {
-        window.alert('Please create your Password')
+        window.alert('Please create your Password');
     } else if (!validatePassword(userPassword)) {
-        window.alert('Password must be at least 8 characters long and include atleast one letter and one number.')
+        window.alert('Password must be at least 8 characters long and include atleast one letter and one number.');
     }
     else {
         const formData = new FormData(registerForm);
@@ -97,8 +102,4 @@ closeResponseMessage.addEventListener('click', () => {
     document.querySelector(".register-response-layer").style.display = 'none';
     document.querySelector(".register-response-two").style.display = 'none';
 })
-// *! prevent user form entering product page directly , check user is authenticate, or redirect to login page directly
-// ** use cookie to check user login and clear cookie after closing browser
-// ** user check login through saving user data in cookie
-// ** initial login page
-//  TODO:  form
+
